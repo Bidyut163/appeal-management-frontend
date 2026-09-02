67054e294820cbe3595f79ece1fc9e88d049d5b1
Bidyut Deka <bidyutranjan464@gmail.com>
2026-09-02 20:00:32 +0530

    feat: add role-based navigation

--- components/layout/main-menu.tsx
+++ components/layout/main-menu.tsx
@@ -3,13 +3,54 @@ import MenuItem from './menu-item';
 import MenuTitle from './menu-title';
 import Link from 'next/link';
 import { SunDimIcon } from 'lucide-react';
+import { useAuthStore } from '@/stores/authStore';
+import type { RoleType } from '@/types/role';
 
 export default function MainMenu() {
-    const menuItems = [
-        // { label: 'My Dashboard', href: '/dashboard' },
-        { label: 'File An Appeal', href: '/appeals/file' },
-        { label: 'List of Appeals', href: '/appeals' },
+    const user = useAuthStore((state) => state.user);
+
+    type MenuItem = {
+        label: string;
+        href: string;
+        roles: RoleType[];
+    };
+
+    const menuItems: MenuItem[] = [
+        {
+            label: 'File an Appeal',
+            href: '/appeals/file',
+            roles: ['APPELLANT'],
+        },
+        {
+            label: 'My Appeals',
+            href: '/appeals',
+            roles: ['APPELLANT'],
+        },
+        {
+            label: 'Appeals',
+            href: '/verifier',
+            roles: ['VERIFIER'],
+        },
+        {
+            label: 'Appeals',
+            href: '/registrar',
+            roles: ['REGISTRAR'],
+        },
+        {
+            label: 'Officials',
+            href: '/admin/users',
+            roles: ['ADMIN'],
+        },
+        {
+            label: 'Create an Official',
+            href: '/admin/users/create',
+            roles: ['ADMIN'],
+        },
     ];
+
+    const allowedItems = menuItems.filter((item) =>
+        item.roles.some((role) => user?.roles.includes(role)),
+    );
 
     return (
         <nav className="bg-muted p-4 border-r overflow-y-auto flex flex-col">
@@ -18,7 +59,7 @@ export default function MainMenu() {
                 <MenuTitle />
             </div>
             <div className="py-4 grow">
-                {menuItems.map((item) => (
+                {allowedItems.map((item) => (
                     <MenuItem key={item.href} href={item.href}>
                         {item.label}
                     </MenuItem>
@@ -28,7 +69,10 @@ export default function MainMenu() {
             <div className="flex gap-2 items-center">
                 <Avatar>
                     <AvatarFallback className="bg-primary text-white">
-                        BD
+                        {user?.name
+                            .split(' ')
+                            .map((word) => word[0])
+                            .join('')}
                     </AvatarFallback>
                 </Avatar>
                 <Link href="/" className="hover:underline">
