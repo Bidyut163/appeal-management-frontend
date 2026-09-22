6227e21085380e5b39f52ac0538938d59adb808f
Bidyut Deka <bidyutranjan464@gmail.com>
2026-09-22 20:05:50 +0530

    fix: redirect root page to login

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-    return (
-        <div>
-            <h1>Cashflow</h1>
-            <Button asChild>
-                <Link href="/login">Login</Link>
-            </Button>
-            <Button asChild>
-                <Link href="/signup">Signup</Link>
-            </Button>
-        </div>
-    );
+    redirect('/login');
 }
