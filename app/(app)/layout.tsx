1ab0d9bdf193a16d76762fede88c6a315431f20c
Bidyut Deka <bidyutranjan464@gmail.com>
2026-09-22 18:20:48 +0530

    fix: contain dashboard main scroll

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -28,7 +28,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
             <MainMenu />
             <section className="flex flex-col min-h-0">
                 {/* <Navbar /> */}
-                <main className="min-h-0 flex-1 overflow-y-auto p-4">
+                <main className="min-h-0 flex-1 overflow-y-auto p-4 contain-paint">
                     {children}
                 </main>
             </section>
